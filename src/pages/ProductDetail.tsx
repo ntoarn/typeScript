9de75039a6948453176e1aefdc0d1e4ba2a8e130
ntoarn <nqton301004@gmail.com>
2024-03-25 01:58:54 +0700

    css

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -43,6 +43,7 @@ const ProductDetail = (props: Props) => {
         </Col>
         <Col md={6}>
           <h2>{product.title  }</h2>
+          {/* <p>{product.description}</p> */}
           <p>{product.description}</p>
           <p>Price: ${product.price}</p>
           <Button variant="primary">Add to Cart</Button>
